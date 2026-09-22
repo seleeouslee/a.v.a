12eaa53ee8a419924a8123d76f111fe9a967c181
seleeouslee <77994147+seleeouslee@users.noreply.github.com>
2026-09-22 17:12:45 -0400

    TEST 5secs

--- js/idle.js
+++ js/idle.js
@@ -1,6 +1,6 @@
 let idleTimer;
 // Set to 5 minutes (5 * 60 * 1000 milliseconds)
-const IDLE_TIMEOUT_MS = 5 * 60 * 1000; 
+const IDLE_TIMEOUT_MS = 5000; 
 
 const idleScreen = document.getElementById('idle-screen');
 
